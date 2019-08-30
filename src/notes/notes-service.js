873ca08c17e5b5fd4bdc45ad17e55ce21aefe29c
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-08-30 13:52:17 -0400

    fix typo note to notes

--- src/notes/notes-service.js
+++ src/notes/notes-service.js
@@ -6,7 +6,7 @@ const NotesService = {
   insertNote(knex, newNote) {
     return knex
       .insert(newNote)
-      .into('note')
+      .into('notes')
       .returning('*')
       .then(rows => {
         return rows[0];
@@ -15,20 +15,20 @@ const NotesService = {
 
   getById(knex, id) {
     return knex
-      .from('note')
+      .from('notes')
       .select('*')
       .where('id', id)
       .first();
   },
 
   deleteNote(knex, id) {
-    return knex('note')
+    return knex('notes')
       .where({ id })
       .delete();
   },
 
   updateNote(knex, id, newNoteFields) {
-    return knex('note')
+    return knex('notes')
       .where({ id })
       .update(newNoteFields);
   }
