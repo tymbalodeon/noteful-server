5c180faefd7d94a39179449a7ea5fddbdb0c2d8c
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-08-30 11:44:26 -0400

    add router files

---
+++ src/folders/folders-router.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const express = require('express');
+const xss = require('xss');
+const FoldersService = require('./folders-service');
+
+const foldersRouter = express.Router();
+const jsonParser = express.json();
+
+const serializeFolder = folder => ({
+  id: folder.id,
+  folder_name: xss(folder.folder_name)
+});
+
+foldersRouter
+  .route('/')
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db');
+    FoldersService.getAllFolders(knexInstance)
+      .then(folders => {
+        res.json(folders.map(serializeFolder));
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { folder_name } = req.body;
+    const newfolder = { folder_name };
+
+    if (folder_name === null) {
+      return res.status(400).json({
+        error: { message: "Missing 'folder_name' in request body" }
+      });
+    }
+
+    FoldersService.insertFolder(req.app.get('db'), newfolder)
+      .then(folder => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${folder.id}`))
+          .json(serializeFolder(folder));
+      })
+      .catch(next);
+  });
+
+foldersRouter
+  .route('/:folder_id')
+  .all((req, res, next) => {
+    FoldersService.getById(req.app.get('db'), req.params.folder_id)
+      .then(folder => {
+        if (!folder) {
+          return res.status(404).json({
+            error: { message: `folder doesn't exist` }
+          });
+        }
+        res.folder = folder;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.json(serializeFolder(res.folder));
+  })
+  .delete((req, res, next) => {
+    FoldersService.deleteFolder(req.app.get('db'), req.params.folder_id)
+      .then(numRowsAffected => {
+        res.status(204).end();
+      })
+      .catch(next);
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { folder_name } = req.body;
+    const folderToUpdate = { folder_name };
+
+    if (folder_name === null) {
+      return res.status(400).json({
+        error: { message: "Missing 'folder_name' in request body" }
+      });
+    }
+
+    FoldersService.updateFolder(
+      req.app.get('db'),
+      req.params.folder_id,
+      folderToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
+
+module.exports = foldersRouter;
---
+++ src/notes/notes-service.js
@@ -0,0 +1,37 @@
+const NotesService = {
+  getAllNotes(knex) {
+    return knex.select('*').from('note');
+  },
+
+  insertNote(knex, newNote) {
+    return knex
+      .insert(newNote)
+      .into('note')
+      .returning('*')
+      .then(rows => {
+        return rows[0];
+      });
+  },
+
+  getById(knex, id) {
+    return knex
+      .from('note')
+      .select('*')
+      .where('id', id)
+      .first();
+  },
+
+  deleteNote(knex, id) {
+    return knex('note')
+      .where({ id })
+      .delete();
+  },
+
+  updateNote(knex, id, newNoteFields) {
+    return knex('note')
+      .where({ id })
+      .update(newNoteFields);
+  }
+};
+
+module.exports = NotesService;
