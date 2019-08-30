3a5da325cfb98b6fdb46b9096c8109f304fa709b
Ahmed <ahmed.max7586@gmail.com>
2019-08-30 12:39:54 -0400

    added test.js

---
+++ test/folders.fixtures.js
@@ -0,0 +1,34 @@
+function makeFolderArray() {
+    return [{
+            id: 1,
+            folder_name: '1st folder',
+            
+        },
+        {
+            id: 2,
+            folder_name: '2nd folder'
+        }
+    ]
+}
+
+function makeMaliciousFolder() {
+    const maliciousFolder = {
+        id: 911,
+        folder_name: 'Naughty naughty very naughty <script>alert("xss");</script>',
+        
+    }
+    const expectedFolder = {
+        ...maliciousFolder,
+        folder_name: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+
+    }
+    return {
+        maliciousFolder,
+        expectedFolder,
+    }
+}
+
+module.exports = {
+    makeFolderArray,
+    makeMaliciousFolder,
+}
---
+++ test/notes.fixtures.js
@@ -0,0 +1,42 @@
+function makeNoteArray() {
+    return [{
+            id: 1,
+            modifed: '2029-01-22T16:28:32.615Z',
+            note_name: 'Sam Gamgee',
+            folder_id: 2,
+            content: 'Sam'
+        },
+        {
+            id: 2,
+            modifed: '2100-05-22T16:28:32.615Z',
+            note_name: 'Peregrin Took',
+            folder_id: 1,
+            content: 'Pippin'
+        }
+    ]
+}
+
+function makeMaliciousNote() {
+    const maliciousNote = {
+        id: 911,
+        note_name: 'Naughty naughty very naughty <script>alert("xss");</script>',
+        modifed: '2100-05-22T16:28:32.615Z',
+        folder_id: 1,
+        content: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+    }
+    const expectedNote = {
+        ...maliciousNote,
+        note_name: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+        content: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`,
+
+    }
+    return {
+        maliciousNote,
+        expectedNote,
+    }
+}
+
+module.exports = {
+    makeNoteArray,
+    makeMaliciousNote,
+}
